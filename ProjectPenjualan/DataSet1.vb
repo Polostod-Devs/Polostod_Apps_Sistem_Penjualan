8a64dc5e5fbdb50bfdea43a15a1f3b351cb5ccf7
SatrioDwiPrabowo <sdprabowo77@gmail.com>
2016-01-31 16:37:27 +0700

    "Polostod Devs Source Code VB.NET v1.0.0-AlphaTest"

---
+++ ProjectPenjualan/DataSet1.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System.Data.SqlServerCe
+Partial Public Class DataSet1
+    Partial Class DataTransaksiDataTable
+
+        Private Sub DataTransaksiDataTable_DataTransaksiRowChanging(sender As Object, e As DataTransaksiRowChangeEvent) Handles Me.DataTransaksiRowChanging
+
+        End Sub
+
+    End Class
+
+    Partial Class ReportDataTable
+
+        Private Sub ReportDataTable_ReportRowChanging(sender As Object, e As ReportRowChangeEvent) Handles Me.ReportRowChanging
+
+        End Sub
+
+    End Class
+
+
+End Class
+
---
+++ ProjectPenjualan/Keterangan.vb
@@ -0,0 +1,3 @@
+﻿Public Class Keterangan
+
+End Class
